d53cb1a2e2146265e8aaa2904622cfc92719f58a
Andrii Skrypnychenko <scrip.a.v@gmail.com>
2024-01-27 23:57:21 +0100

    added playwright config

--- tests/chrome.test.js
+++ tests/chrome.test.js
@@ -1,4 +1,11 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
 
 test('FinPrime Website Test', async ({ page }) => {
   await page.goto('https://www.finprime.pl/');
